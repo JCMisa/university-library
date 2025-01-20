f24c5e432734f15565530b8fbabfb57b59817546
MisaJC <johncarlomisa399@gmail.com>
2025-01-20 18:22:57 +0800

    finish the style of book list and book card in home page

---
+++ app/(root)/page.tsx
@@ -0,0 +1,16 @@
+import BookList from "@/components/custom/BookList";
+import BookOverview from "@/components/custom/BookOverview";
+import { sampleBooks } from "@/constants";
+
+const Home = () => (
+  <>
+    <BookOverview {...sampleBooks[0]} />
+    <BookList
+      title="Latest Books"
+      books={sampleBooks}
+      containerClassName="mt-28"
+    />
+  </>
+);
+
+export default Home;
