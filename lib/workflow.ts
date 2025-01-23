f89761db864a948d1f4eb2d1402281313dab8a0e
MisaJC <johncarlomisa399@gmail.com>
2025-01-23 12:57:35 +0800

    fix email domain

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -26,7 +26,7 @@ export const sendEmail = async ({
       provider: resend({ token: config.env.resendToken }),
     },
     body: {
-      from: "JCM <jcm@gmail.com>",
+      from: "JCM <contact@adrianjsmastery.com>",
       to: [email],
       subject,
       html: message,
